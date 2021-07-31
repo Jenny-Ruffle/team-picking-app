f53c0717352f98909b2d656ce67147ad4b963bb5
Jenny Ruffle <rufflejennifer@gmail.com>
2021-07-31 23:23:39 +0100

    task: initial setup

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2 className="heading">
+          Team picking app
+        </h2>
+      </header>
+      <div className="App-content">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
